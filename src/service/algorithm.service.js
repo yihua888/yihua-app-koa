fc0fda1339a32323e6fb3a513c1abd328e3d728d
yihua <yihua@yihua.com>
2022-12-01 23:30:49 +0800

    解决算法查询BUG

--- src/service/algorithm.service.js
+++ src/service/algorithm.service.js
@@ -1,19 +1,19 @@
 const connection = require('../app/database')
 
 class AlgorithmService {
-    async createAlgorithmType(typeName,typeCode,id){
+    async createAlgorithmType (typeName, typeCode, id) {
         const statement = `insert into tb_algorithm_type (algorithm_type,type_name,id) values(?,?,?);`
-        const [result] = await connection.execute(statement, [typeCode,typeName,id])
+        const [result] = await connection.execute(statement, [typeCode, typeName, id])
         return result
     }
 
-    async getAlgorithmTypes(){
+    async getAlgorithmTypes () {
         const statement = `select algorithm_type typeCode,type_name typeName, id from  tb_algorithm_type;`
         const [result] = await connection.query(statement)
         return result
     }
 
-    async getAlgorithmTypeByName(name){
+    async getAlgorithmTypeByName (name) {
         const statement = `select algorithm_type typeCode,type_name  typeName , id from  tb_algorithm_type where type_name = ?;`
         const [result] = await connection.execute(statement, [name])
         return result[0]
@@ -37,17 +37,20 @@ class AlgorithmService {
         return result
     }
 
-    async getAlgorithms (algorithmName , algorithmType ,limit, offset) {
+    async getAlgorithms (algorithmName, algorithmType, limit, offset) {
         let condition = ''
         const argumentsList = []
         algorithmName && (condition += ` and algorithm_name like concat('%', ?, '%')`) && argumentsList.push(algorithmName);
-        algorithmType && (condition += ` and algorithm_type like concat('%', ?, '%')`) && argumentsList.push(algorithmType);
+        if (algorithmType) {
+            condition += ` and a.type_id=? `
+            argumentsList.push(algorithmType);
+        }
         const statement = `select a.name name,a.label label,a.info info,a.id id,atype.type_name type
         from tb_algorithm a
-        left join tb_algorithm_type atype on atype.id=a.type_id` + condition + ` LIMIT ? , ?;`
-        const statement1 = `select count(*) total from tb_algorithm a left join tb_algorithm_type atype on atype.id=a.type_id` + condition 
-        const [rst] = await connection.execute(statement1 , [...argumentsList])
-        const [data] = await connection.execute(statement, [...argumentsList , offset, limit])
+        left join tb_algorithm_type atype on atype.id=a.type_id where 1=1 ` + condition + ` LIMIT ? , ?;`
+        const statement1 = `select count(*) total from tb_algorithm a left join tb_algorithm_type atype on atype.id=a.type_id where 1=1 ` + condition
+        const [rst] = await connection.execute(statement1, [...argumentsList])
+        const [data] = await connection.execute(statement, [...argumentsList, offset, limit])
         return {
             data,
             total: rst[0].total
@@ -66,7 +69,7 @@ class AlgorithmService {
         where a.id = ?;
         `
         try {
-            const [rst] = await connection.execute(statement , [algorithmId])
+            const [rst] = await connection.execute(statement, [algorithmId])
             return rst[0]
         } catch (error) {
             console.log(error);
