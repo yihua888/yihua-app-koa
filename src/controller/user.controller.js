16774461136e966bf48b56ce9a4fe726f10365c1
pengyinghao <pengyinghao@pengyinghao>
2022-12-12 18:05:26 +0800

    头像上传完成

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,30 +1,51 @@
-const userService = require('../service/user.service');
+const userService = require("../service/user.service");
+const fs = require("fs");
+const createID = require("../utils/createId");
 
-class UserController{
-  async create(ctx,next){
+class UserController {
+  async create(ctx, next) {
     // 获取用户传递的参数
     const user = ctx.request.body;
     // 查询数据
-    const rst = await userService.create(user)
+    const rst = await userService.create(user);
     // 返回数据
     ctx.body = rst;
   }
 
-  async getUsers(ctx,next){
-    const { username, limit, offset } = ctx.request.query
-    const rst = await userService.getUsers(username, limit, offset)
-    ctx.body = rst
+  async getUsers(ctx, next) {
+    const { username, limit, offset } = ctx.request.query;
+    const rst = await userService.getUsers(username, limit, offset);
+    ctx.body = rst;
   }
 
-  async getUserById(ctx,next){
-    const { userId } = ctx.request.params
-    const rst = await userService.getUserById(userId)
-    ctx.body = rst
+  async getUserById(ctx, next) {
+    const { userId } = ctx.request.params;
+    const rst = await userService.getUserById(userId);
+    ctx.body = rst;
   }
 
-  async updateUser(ctx,next){
-    console.log(ctx.request);
+  async updateUser(ctx, next) {
+    const { id: userId, username, roleId, avatarId: aId } = ctx.request.body;
+    const files = ctx.request.files;
+    const flag = aId === "null" || !aId;
+    const avatarId = flag ? createID() : aId;
+    if (Object.keys(files)[0]) {
+      // 保存文件地址到avatar
+      const avatarUrl =
+        "upload/avatar/" + files[Object.keys(files)[0]].newFilename;
+      const filename = files[Object.keys(files)[0]].originalFilename;
+      if (!flag) await userService.upDateAvatar(avatarUrl, aId, filename);
+      else await userService.createAvatar(avatarUrl, avatarId, filename);
+    }
+    // 更新其余信息
+    await userService.updateUser(
+      username,
+      userId,
+      roleId,
+      Object.keys(files)[0] ? avatarId : null
+    );
+    ctx.body = "修改成功";
   }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,13 +1,27 @@
-const Router = require('koa-router')
+const Router = require("koa-router");
+const { koaBody }  = require("koa-body");
+const path = require('path')
+const createID = require("../utils/createId");
 
-const { create, getUsers, getUserById, updateUser} = require('../controller/user.controller')
-const {verifyUser, handlePassword} = require('../middleware/user.middleware')
-const userRouter = new Router({prefix: '/users'})
+const {
+  create,
+  getUsers,
+  getUserById,
+  updateUser,
+} = require("../controller/user.controller");
+const { verifyUser, handlePassword } = require("../middleware/user.middleware");
+const userRouter = new Router({ prefix: "/users" });
 
 // 注册用户
-userRouter.post('/', verifyUser , handlePassword, create)
-userRouter.patch('/', updateUser)
-userRouter.get('/' , getUsers)
-userRouter.get('/:userId' , getUserById)
+userRouter.post("/", verifyUser, handlePassword, create);
+userRouter.patch("/" , koaBody({
+    "multipart":true,
+    formidable:{
+        uploadDir: path.resolve(__dirname,'../../upload/avatar'),
+        filename:(name,ext,part) => createID() + path.extname(part.name)
+    }
+}),updateUser);
+userRouter.get("/", getUsers);
+userRouter.get("/:userId",  getUserById);
 
-module.exports = userRouter
+module.exports = userRouter;
