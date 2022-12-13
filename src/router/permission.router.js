51533c5f410ff2e8c9f90f1e08d6b0b6b69da3c1
yihua <yihua@yihua.com>
2022-12-13 23:33:30 +0800

    权限添加部分接口

--- src/router/permission.router.js
+++ src/router/permission.router.js
@@ -1,11 +1,19 @@
-const Router = require("koa-router");
+const Router = require('koa-router')
 
-const { verifyAuth } = require("../middleware/auth.middleware");
-const { getPermission } = require("../controller/permission.controller");
+const { verifyAuth } = require('../middleware/auth.middleware')
+const { getPermission, getPermissionList, create, update, remove } = require('../controller/permission.controller')
 
-const userRouter = new Router({ prefix: "/permission" });
+const userRouter = new Router({ prefix: '/permission' })
 
 // 获取权限
-userRouter.get("/", verifyAuth, getPermission);
+userRouter.get('/', verifyAuth, getPermission)
+// 获取权限列表
+userRouter.get('/list', getPermissionList)
+// 新增权限
+userRouter.post('/', verifyAuth, create)
+// 更新
+userRouter.patch('/', verifyAuth, update)
+// 删除
+userRouter.delete('/:id', verifyAuth, remove)
 
-module.exports = userRouter;
+module.exports = userRouter
